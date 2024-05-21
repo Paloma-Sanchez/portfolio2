7a09f8dfbea1d43bfd894f0005fea90104a37089
Paloma <karpaloma@hotmail.com>
2024-05-21 16:50:10 +0300

    fix:worked on responsive for projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,12 +1,12 @@
 <template>
     <main>
         <p
-            class="font-Archivo text-8xl text-[#1B4572] dark:text-[#DCECFF] font-extrabold  mt-32 ml-32 "
+            class="font-Archivo text-5xl lg:text-6xl xl:text-8xl text-[#1B4572] dark:text-[#DCECFF] font-extrabold text-center lg:text-left mt-16  lg:mt-20 lg:ml-20  xl:ml-32 mb-10 lg:mb-0"
         >
             {{ $t ("projects.title")}}
         </p>
         <projects-video-grid
-            class="ml-[45%] -mt-36   "
+            class="lg:ml-[37%] xl:ml-[45%] lg:-mt-28 xl:-mt-36 flex justify-center"
         />
     </main>
 
