4f4179c61874f2e16579c4585f9741b2efd2d63b
Paloma <karpaloma@hotmail.com>
2024-05-28 19:21:30 +0300

    test3: idem

--- components/Projects/VideoGrid.vue
+++ components/Projects/VideoGrid.vue
@@ -15,7 +15,7 @@
                     @mouseleave="stopVideo(index)"
                 >
                     <source
-                        :src="'../../assets/video/pmail.webm'"
+                        src="../../assets/video/pmail.webm"
                         type="video/webm; codecs=vp9,opus"
                     />
                     <source 
