f74f426c2885e6115291a122b36899dbdc428b46
Paloma <karpaloma@hotmail.com>
2024-06-02 12:50:14 +0300

    fix: worked on about responsive display

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,13 @@
 <template>
     <main>
         <div 
-            class="h-[85vh] flex flex-col  "
+            class="h-[85vh] xl:h-[80vh] flex flex-col justify-between "
             :class="[
-                    {
-                        'justify-between':!hiddenScroll
-                    }
+
                 ]"
         >
-            <h1
+            <div>
+                <h1
                 class="bg-gradient-to-b from-white dark:to-black to-[#3B4856] bg-clip-text text-transparent uppercase text-[70px] sm:text-[124px] md:text-[150px] lg:text-[202px] xl:text-[252px] desktop:text-[264px] font-bold text-center mt-16 sm:mt-16 md:mt-8 lg:mt-4 opacity-0 animate-slideUpImmediate  ">
                 {{ $t("about.title") }}
             </h1>
@@ -32,6 +31,8 @@
                     </div>
                 </div>
             </div>
+            </div>
+            
             <about-scroll-flat 
                 :class="[
                     {
@@ -40,11 +41,13 @@
                 ]"
             />
         </div>
-
-        <div class=" flex justify-center mb-4">
+        <div
+            class=" h-[90vh] "
+        >
+                <div class=" flex justify-center mb-8">
             <div class="grid grid-cols-1 md:grid-cols-3 min-w-[80%] max-w-[1352px]">
                 <h2 data-aos="fade-up"
-                    class="text-center  uppercase  sm:ml-20 sm:mr-3 mb-16 sm:mb-20 sm:col-start-2 col-span-2  bg-gradient-to-b from-white dark:to-black to-[#3B4856] bg-clip-text text-transparent text-6xl lg:text-[74px] xl:text-[100px] font-bold ">
+                    class="text-center  uppercase  sm:ml-16  sm:mr-3 mb-16 sm:mb-20 sm:col-start-2 col-span-2  bg-gradient-to-b from-white dark:to-black to-[#3B4856] bg-clip-text text-transparent text-4xl sm:text-6xl lg:text-[74px] xl:text-[100px] font-bold ">
                     Technologies
                 </h2>
                 <div class="sm:col-span-2 sm:row-start-2 ">
@@ -57,6 +60,8 @@
 
             </div>
         </div>
+        </div>
+
     </main>
 </template>
 <script setup lang="ts">
